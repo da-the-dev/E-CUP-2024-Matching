502392a395fa627207284d9c8ff365f51d43648a
Alexey Tkachenko <a.tkachenko@innopolis.university>
2024-08-28 15:10:27 +0300

    Added bert transformer

--- src/transformations.py
+++ src/transformations.py
@@ -1,7 +1,13 @@
 __all__ = ["categories_transformer", "attr_transformer"]
 
 import json
+from sklearn.decomposition import PCA
+import torch
+import random
+import numpy as np
+import torch.nn as nn
 from sklearn.base import BaseEstimator, TransformerMixin
+
 
 
 class categories_transformer(BaseEstimator, TransformerMixin):
@@ -46,3 +52,30 @@ class attr_transformer(BaseEstimator, TransformerMixin):
 
         return X
 
+
+class bert_64_transformer(BaseEstimator, TransformerMixin):
+    def __init__(self, model, feature: str):
+        super().__init__()
+
+        self.model = model
+        self.feature = feature
+        self.pca = PCA(n_components=64)
+
+        # self.device = "cuda" if torch.cuda.is_available() else "cpu"
+        # self.projection_layer = nn.Linear(768, 64).to(self.device)
+
+    def transform(self, X):
+        # Fix seeds for reproducibility
+        def set_seed(seed):
+            torch.manual_seed(seed)
+            torch.cuda.manual_seed_all(seed)
+            np.random.seed(seed)
+            random.seed(seed)
+            torch.backends.cudnn.deterministic = True
+
+        set_seed(42)
+        
+        # Apply the encoding function to the specified feature
+        X[self.feature] = list(self.model.encode(X[self.feature]))
+
+        return X
